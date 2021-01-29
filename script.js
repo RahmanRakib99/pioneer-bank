c644d0ac7bcd4288a96701e8611885d04f08be28
unknown <rahmanrakibur6589@gmail.com>
2021-01-29 00:45:10 -0800

    updated

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-//1: Login Button click handle
-
+//login btn click handle
 const loginBtn=document.getElementById("login-button");
 loginBtn.addEventListener("click",function(){
-
-   const loginArea=document.getElementById("login-area");
+    const loginArea=document.getElementById("login-area");
    loginArea.style.display="none";
 
    const transactionArea=document.getElementById("transaction-area");
-   transactionArea.style.display="block";
+transactionArea.style.display="block";
 })
+//deposit btn  click handle
+const depositBtn=document.getElementById("addDeposit");
+depositBtn.addEventListener("click",function(){
+     const depositAmount=document.getElementById("depositAmount").value;
+    const depositNumber=parseFloat(depositAmount);
 
-//2:deposite button click handle
-const getDepoBtn=document.getElementById("Deposite-btn");
-getDepoBtn.addEventListener("click",function(){
-    const getInputValue=document.getElementById("deposite-input-text").Value;
-    const getInputNumber= parseFloat(getInputValue);
+    const currentDeposit=document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber=parseFloat(currentDeposit);
+    const totalDeposit=depositNumber+currentDepositNumber;
+    document.getElementById("currentDeposit").innerText=totalDeposit;
+    //samevhabe balance add
+    const currentBalance=document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber=parseFloat(currentBalance);
+    const totalBalance=currentBalanceNumber+depositNumber;
+    document.getElementById("currentBalance").innerText=totalBalance;
 
-    const currentDeposite=document.getElementById("currentDeposite").innerText;
-    const currentDepositeNumber=parseFloat(currentDeposite);
-const totalDeposite=getInputNumber+currentDepositeNumber;
-   
-document.getElementById("currentDeposite").innerText=totalDeposite;
+
+    document.getElementById("depositAmount").value="";//depo input ke empty korar jnnw; 
+})
+// withdraw Btn click handle
+const withdrawBtn=document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click",function(){
+     const withdrawAmount=document.getElementById("withdrawAmount").value;
+     const withdrawAmountNumber=parseFloat(withdrawAmount);
+
+     const currentWithdraw=document.getElementById("currentWithdraw").innerText;
+     const currentWithdrawNumber=parseFloat( currentWithdraw);
+     const totalWithdraw=withdrawAmountNumber+currentWithdrawNumber;
+     document.getElementById("currentWithdraw").innerText=totalWithdraw;
+     //samevhabe balance theke remove hobe
+     const currentWithdrawBalance=document.getElementById("currentWithdraw").innerText;
+     const currentWithdrawBalanceNumber=parseFloat(currentWithdrawBalance);
+     const totalWithdrawBalance=currentWithdrawBalanceNumber-withdrawAmountNumber;
+     document.getElementById("currentWithdraw").innerText= totalWithdrawBalance;
+
+
+
+
+
+
+
+     document.getElementById("withdrawAmount").value="";
 })
